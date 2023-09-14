41ca3ce80e3ef535a2c083f8018a98115e0b3658
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-14 10:57:16 +0200

    Add project files.

---
+++ Constructors/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public int Pages;
+
+        //comstructor with methods
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            Title = aTitle;
+            Author = aAuthor;
+            Pages = aPages;
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("\"The Power of Positive Thinking\"", "Norman Vincent Pale", 107);
+            Book book2 = new Book("\"Forgive YourSelf\"", "Jared Singer", 100);
+            Book book3 = new Book("\"Finding Me\"", "Viola Davis", 304);
+            Book book4 = new Book("\"LORD Of The Flies\"", "William Golding", 318);
+            Console.WriteLine("The first book title " + book1.Title);
+            Console.WriteLine("The 2nd book title " + book2.Title);
+            Console.WriteLine("The 3rd book title " + book3.Title);
+            Console.WriteLine("The 4th book title " + book4.Title);
+
+            Console.ReadLine();
+        }
+    }
+}
